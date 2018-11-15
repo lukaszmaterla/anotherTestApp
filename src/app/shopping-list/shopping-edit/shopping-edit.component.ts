1514a364559798f6d44933e70a1906792f3d2b38
lukaszmaterla <lukasz.materla91@gmail.com>
2018-11-15 22:08:22 +0100

    Added possible to add new ingredients to list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredients = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  saveIngred() {
+    const el = this.nameInputRef.nativeElement;
+    const ingredients2 = new Ingredient(el.value, el.value);
+    this.ingredients.emit(ingredients2);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,5 +15,8 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
+  onAdd(event: Ingredient) {
+    this.ingredients.push(event);
+  }
 
 }
