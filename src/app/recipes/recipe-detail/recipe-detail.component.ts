0a9b7b93a0108003c3f209ba19ff930f2d3f016b
lukaszmaterla <lukasz.materla91@gmail.com>
2018-11-20 21:19:22 +0100

    Inject one service into another using Injectable feature

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit() {
   }
   onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.details.ingredients);
   }
 
 }
