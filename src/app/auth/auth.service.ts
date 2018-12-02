cd5a601d6e4d3add5088c3d239148c4af140ca0a
lukaszmaterla <lukasz.materla91@gmail.com>
2018-12-02 18:16:59 +0100

    Created SingIn from working

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,15 @@ export class AuthService {
     firebase.auth().createUserWithEmailAndPassword(email,password)
       .catch(
         (error) => console.log(error)
+      );
+  }
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
       )
+      .catch(
+        error => console.log(error)
+      );
   }
 }
