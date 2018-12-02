4ca4773712286e056234f2cd0415c3272b3bc3a4
lukaszmaterla <lukasz.materla91@gmail.com>
2018-12-02 18:31:04 +0100

    Added Auth Authentication to get and save recipes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import * as firebase from 'firebase/app';
 
 export class AuthService {
+
+  token: string;
+
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email,password)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
         (error) => console.log(error)
       );
@@ -10,10 +13,23 @@ export class AuthService {
   signInUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response => console.log(response)
+        response => {
+         firebase.auth().currentUser.getIdToken()
+           .then(
+             (token: string) => this.token = token
+           );
+        }
       )
       .catch(
         error => console.log(error)
       );
   }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then(
+      (token: string) => this.token = token
+    );
+    return this.token;
+  }
+
 }
